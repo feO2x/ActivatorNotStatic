44429a3ecfa8641bcd4fc6f24ed4fc7379824ad8
Kenny Pflug <kenny.pflug@live.de>
2017-08-15 21:44:10 +0200

    added examples that typeof(Activator).IsAbstract is false

---
+++ DotNet/ActivatorTypeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace DotNet
+{
+    public sealed class ActivatorTypeTests
+    {
+        [Fact]
+        public void ActivatorNotAbstractDotNet()
+        {
+            var activatorType = typeof(Activator);
+
+            Assert.True(activatorType.IsClass);
+            Assert.True(activatorType.IsSealed);
+            Assert.True(activatorType.IsAbstract);
+        }
+    }
+}
---
+++ DotNetCore/ActivatorTypeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace DotNetCore
+{
+    public sealed class ActivatorTypeTests
+    {
+        [Fact]
+        public void ActivatorNotAbstractDotNetCore()
+        {
+            var activatorType = typeof(Activator);
+
+            Assert.True(activatorType.IsClass);
+            Assert.True(activatorType.IsSealed);
+            Assert.True(activatorType.IsAbstract);
+        }
+    }
+}
